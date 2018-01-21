ed2f55b80052ed05e55d34a099c9de6342515de8
ayubov <alvi.ayubov@yandex.ru>
2018-01-21 15:59:29 +0300

    add project

---
+++ src/weather/App.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import WeatherIcon from './components/WeatherIcon';
+import WeatherDetails from './components/WeatherDetails';
+
+class App extends Component {
+    state = {
+        icon: '',
+        time: 1,
+        city: '',
+        temperature:'',
+        weatherCode: '',
+        fetching: true
+    }
+
+    componentDidMount() {
+        this.fetchIP();
+    }
+
+    fetchWeatherData = city => {
+        const baseUrl = 'http://api.openweathermap.org';
+        const path = '/data/2.5/weather';
+        const appID = '459fc1107972f9ca8e39abe5f2583722';
+        const query = `units=metric&lang=ru&appid=${appID}`;
+
+        fetch(`${baseUrl}${path}?q=${city}&${query}`)
+            .then(response => response.json())
+            .then(data => {
+                const date = new Date();
+                const time = date.getHours();
+
+                this.setState({
+                    time,
+                    city,
+                    temperature: Math.round(data.main.temp),
+                    weatherCode: data.weather[0].id,
+                    fetching: false
+                });
+            })
+            .catch(e => console.error(e));
+    }
+
+    fetchIP = () => {
+        fetch('//freegeoip.net/json/')
+            .then(response => response.json())
+            .then(({ city }) => this.fetchWeatherData(city))
+            .catch(e => console.log(e));
+    }
+
+
+
+    render() {
+        const { fetching, icon, time, city, temperature, weatherCode } = this.state;
+        return fetching ? 
+            <div className="app">Loading...</div>
+            :
+            <div className="app" data-hour={time}>
+                <WeatherIcon
+                    icon={icon}
+                    weatherCode={weatherCode}
+                    time={time}
+                />
+                <WeatherDetails
+                    city={city}
+                    temperature={temperature}
+                />
+            </div>
+    }
+}
+
+export default App;
